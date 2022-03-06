8a69105d3d0cd394eef6e1acf22b34e93848ebfa
Kenny Luque <kenny.luque.t@uni.pe>
2022-03-06 10:56:54 -0500

    commit2

--- server.py
+++ server.py
@@ -17,7 +17,12 @@ def tableroAjedrez_mxn(m,n):
 
 @app.route('/<m>/<n>/<color1>')
 def tableroAjedrez_mxn_color1(m, n, color1):
-    return render_template('mxnxcolor.html', x=int(m), y=int(n), color="color1")
+    return render_template('mxnxcolor1.html', x=int(m), y=int(n), color1=str(color1))
+
+
+@app.route('/<m>/<n>/<color1>/<color2>')
+def tableroAjedrez_mxn_color1_color2(m, n, color1,color2):
+    return render_template('mxnxcolor1xcolor2.html', x=int(m), y=int(n), color1=str(color1), color2=str(color2))
 
 if __name__ == "__main__":
     app.run(debug=True)
